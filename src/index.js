8f7684f1b5e74719e8de3add685f3170bdab90bb
Mortimel Ojeda <ojedamorti@gmail.com>
2023-09-13 16:51:59 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-//import { War } from "./war";
 
 /* Create array(deck) of objects(cards )*/
 const deck = [
@@ -68,6 +67,13 @@ const deck = [
     {value: 14, img: "AS.png"},
 ];
 
+/* Function for animating the players' cards */
+function animateCards(p1AnimatedCard, p2AnimatedCard, deck1Top=null, deck2Top=null) {
+    p1AnimatedCard.classList.add("p1-card-animation");
+    p2AnimatedCard.classList.add("p2-card-animation");
+
+}
+
 /* Implement Knuth Shuffle */
 function shuffle(deck) {
     let currentIndex = deck.length;
@@ -86,6 +92,10 @@ function shuffle(deck) {
 const shuffledDeck = shuffle(deck);
 const war = new War(deck.slice(0,shuffledDeck.length/2), deck.slice(shuffledDeck.length/2));
 
+/* Get the id of each players'card to animate */
+let p1AnimatedCard = document.getElementById("p1-animation-card");
+let p2AnimatedCard = document.getElementById("p2-animation-card");
+
 /* Get the id of the html element that contains each players' card count */
 let p1CardCount = document.getElementById("p1-card-count");
 let p2CardCount = document.getElementById("p2-card-count");
@@ -102,60 +112,62 @@ window.addEventListener("load", () => {
 
 let battleButton = document.getElementById("battle-btn");
 battleButton.addEventListener("click", () => {
-    document.querySelector("#p1-card").innerHTML = `<img class="card-img" src="./assets/images/${war.deck1[0].img}" alt="${war.deck1[0].img.slice(0,-4)}">`
-    document.querySelector("#p2-card").innerHTML = `<img class="card-img" src="./assets/images/${war.deck2[0].img}" alt="${war.deck2[0].img.slice(0,-4)}">`
+    animateCards(p1AnimatedCard, p2AnimatedCard)
 
-    switch (war.compareValues(war.deck1[0], war.deck2[0])) {
-        case 1:
-            war.deck1.push(war.deck1.splice(0,1)[0]);
-            war.deck1.push(war.deck2.splice(0,1)[0]);
-            if ( war.warArray1.length !== 0) {
-                war.deck1.push(...war.warArray1)
-                war.deck1.push(...war.warArray2)
-                war.warArray1 = []
-                war.warArray2 = []
-            }
-            p1CardCount.innerHTML = war.deck1.length;
-            p2CardCount.innerHTML = war.deck2.length;
-            break;
-        case -1:
-            war.deck2.push(war.deck1.splice(0,1)[0]);
-            war.deck2.push(war.deck2.splice(0,1)[0]);
-            if ( war.warArray2.length !== 0) {
-                war.deck2.push(...war.warArray1)
-                war.deck2.push(...war.warArray2)
-                war.warArray1 = []
-                war.warArray2 = []
-            }
-            p1CardCount.innerHTML = war.deck1.length;
-            p2CardCount.innerHTML = war.deck2.length;
-            break;
-        case 0:
-            for (let i = 0; i < 4; i++) {
-                const node1 = document.createElement("img")
-                node1.setAttribute("src", "")
-                p1WarArray.appendChild()
-                p2WarArray.appendChild()
-                war.warArray1.push(war.deck1.splice(0,1)[0])
-                war.warArray2.push(war.deck2.splice(0,1)[0])
-                p1CardCount.innerHTML = war.deck1.length;
-                p2CardCount.innerHTML = war.deck2.length;
-            }
-            break;
-    }
+    // document.querySelector("#p1-card").innerHTML = `<img class="card-img" src="./assets/images/${war.deck1[0].img}" alt="${war.deck1[0].img.slice(0,-4)}">`
+    // document.querySelector("#p2-card").innerHTML = `<img class="card-img" src="./assets/images/${war.deck2[0].img}" alt="${war.deck2[0].img.slice(0,-4)}">`
 
-    if (war.checkForWinner()) {
-        switch (war.checkForWinner()) {
-            case 1:
-                alert("Player 1 Wins!")
-                break;
-            case -1:
-                alert("Player 2 Wins!")
-                break;
-            case 0:
-                alert("It's a tie.")
-                break;
-        }
-    }
+    // switch (war.compareValues(war.deck1[0], war.deck2[0])) {
+    //     case 1:
+    //         war.deck1.push(war.deck1.splice(0,1)[0]);
+    //         war.deck1.push(war.deck2.splice(0,1)[0]);
+    //         if ( war.warArray1.length !== 0) {
+    //             war.deck1.push(...war.warArray1)
+    //             war.deck1.push(...war.warArray2)
+    //             war.warArray1 = []
+    //             war.warArray2 = []
+    //         }
+    //         p1CardCount.innerHTML = war.deck1.length;
+    //         p2CardCount.innerHTML = war.deck2.length;
+    //         break;
+    //     case -1:
+    //         war.deck2.push(war.deck1.splice(0,1)[0]);
+    //         war.deck2.push(war.deck2.splice(0,1)[0]);
+    //         if ( war.warArray2.length !== 0) {
+    //             war.deck2.push(...war.warArray1)
+    //             war.deck2.push(...war.warArray2)
+    //             war.warArray1 = []
+    //             war.warArray2 = []
+    //         }
+    //         p1CardCount.innerHTML = war.deck1.length;
+    //         p2CardCount.innerHTML = war.deck2.length;
+    //         break;
+    //     case 0:
+    //         for (let i = 0; i < 4; i++) {
+    //             const node1 = document.createElement("img")
+    //             node1.setAttribute("src", "./")
+    //             p1WarArray.appendChild()
+    //             p2WarArray.appendChild()
+    //             war.warArray1.push(war.deck1.splice(0,1)[0])
+    //             war.warArray2.push(war.deck2.splice(0,1)[0])
+    //             p1CardCount.innerHTML = war.deck1.length;
+    //             p2CardCount.innerHTML = war.deck2.length;
+    //         }
+    //         break;
+    // }
+
+    // if (war.checkForWinner()) {
+    //     switch (war.checkForWinner()) {
+    //         case 1:
+    //             alert("Player 1 Wins!")
+    //             break;
+    //         case -1:
+    //             alert("Player 2 Wins!")
+    //             break;
+    //         case 0:
+    //             alert("It's a tie.")
+    //             break;
+    //     }
+    // }
 })
 
